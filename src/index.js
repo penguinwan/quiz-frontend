c49bc340bbc9b44ddcfea99e41d64c7f5b40100e
penguinwan <penguin_wan@yahoo.com.sg>
2020-11-15 15:08:58 +0100

    optimized

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Quiz from './Quiz';
-import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <Quiz />,
   document.getElementById('root')
 );
